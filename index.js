1cbf1bdc1953976540f25f8085bca2e25c9d2419
Kevinmrozek <kmrozek0859@gmail.com>
2023-05-08 12:24:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const result = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return result;
+}
+function fuzzyMatch(drivers, partialString) {
+    return drivers.filter(driver => driver.startsWith(partialString));
+
+}
+function matchName(drivers, name){
+
+    const result = drivers.filter(driver => driver.name === name);
+    
+    return result;
+}
